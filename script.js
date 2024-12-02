5448a35db394e425fdd2a55eff09f4ad2b8382e4
The Beast <pradhanaditya555@gmail.com>
2024-12-03 00:35:59 +0530

    updating logic

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let userScore = 0;
 let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
 //taking user input
 choices.forEach((choice) => {
     choice.addEventListener("click", () => {
@@ -9,7 +13,7 @@ choices.forEach((choice) => {
         playGame(userChoice);
     } );
 });
-//compiling user choice & comp choice 
+//comparing user choice & comp choice 
 const playGame = (userChoice) => {
     console.log(`user has selected ${userChoice}`);
     const compChoice = genCompChoice();
@@ -18,7 +22,17 @@ const playGame = (userChoice) => {
     //creating draw game logic
 if (userChoice === compChoice) {
     drawGame();
-};
+}else {
+    let userWin = true;
+    if (userChoice === "rock"){
+        userWin = compChoice === "paper" ? false:true;
+    }else if (userChoice === "paper"){
+        userWin = compChoice === "scissors" ? false : true; 
+    }else {
+        userWin = compChoice === "rock" ? false : true;
+    };
+    showWinner(userWin,userChoice,compChoice);
+}
 };
 //generating random index for computer choice
 const genCompChoice = () => {
@@ -31,4 +45,28 @@ const genCompChoice = () => {
 //creating funtion for draw game
 const drawGame = () =>{
     console.log("game was draw");
+    msg.innerText = "======= This Round was Draw =======";
+    msg.style.backgroundColor = "Yellow";
+    msg.style.color = "blue";
 };
+//declaring winner
+const showWinner = (userWin,userChoice,compChoice) => {
+    if (userWin){
+        msg.innerText = `*** Users's ${userChoice} beats computer choice ${compChoice} ***`;
+        msg.style.backgroundColor = "green";
+        msg.style.color = "yellow";
+
+        //updating Score
+        userScore++;
+        userScorePara.innerText = userScore;
+        userScorePara.style.color = "green";
+    }else {
+        msg.innerText = `*** Computer's ${compChoice} beats User choice ${userChoice} ***`;
+        msg.style.backgroundColor = "#d00000";
+        msg.style.color = "yellow";
+        //updating score
+        compScore++;
+        compScorePara.innerText = compScore;
+        compScorePara.style.color = "red";
+    }
+}
